15bf0b4f48e788d312bc2cba7d2398d9a74b125a
Christian Wende <christian.wende@devboost.de>
2013-07-04 18:08:24 +0200

    initial import of test project

---
+++ de.monkeyworks.natspec.library.swtbot.tests/src/de/monkeyworks/natspec/library/swtbot/tests/_NatSpecTemplate.java
@@ -0,0 +1,22 @@
+package de.monkeyworks.natspec.library.swtbot.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class _NatSpecTemplate {
+
+	
+	@Test
+	public void executeScript() throws Exception {
+		/* @MethodBody */
+	}
+
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void shutdown() {
+	}
+}
