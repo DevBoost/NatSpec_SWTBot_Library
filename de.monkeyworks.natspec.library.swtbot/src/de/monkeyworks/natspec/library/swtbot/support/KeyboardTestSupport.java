fd715c0145649205ddd27c3bd6b38f29e962e73c
Stefan Hennig <stefan@monkey-works.de>
2013-10-23 19:33:34 +0200

    Added support for pressing the Enter key.
    Signed-off-by: Stefan Hennig <stefan@monkey-works.de>

--- de.monkeyworks.natspec.library.swtbot/src/de/monkeyworks/natspec/library/swtbot/support/KeyboardTestSupport.java
+++ de.monkeyworks.natspec.library.swtbot/src/de/monkeyworks/natspec/library/swtbot/support/KeyboardTestSupport.java
@@ -1,13 +1,23 @@
 package de.monkeyworks.natspec.library.swtbot.support;
 
 import org.eclipse.jface.bindings.keys.KeyStroke;
+import org.eclipse.swt.SWT;
 import org.eclipse.swtbot.swt.finder.keyboard.Keyboard;
 import org.eclipse.swtbot.swt.finder.keyboard.KeyboardFactory;
 
 public class KeyboardTestSupport {
 
+	private static Keyboard keyboard;
+	
+	static {
+		keyboard = KeyboardFactory.getSWTKeyboard();
+	}
+	
 	public static void pressKey(char key) {
-		Keyboard k = KeyboardFactory.getSWTKeyboard();
-		k.pressShortcut(KeyStroke.getInstance(key, 0));
+		keyboard.pressShortcut(KeyStroke.getInstance(key, 0));
+	}
+	
+	public static void pressEnter() {
+		keyboard.pressShortcut(SWT.CR, SWT.LF);
 	}
 }
